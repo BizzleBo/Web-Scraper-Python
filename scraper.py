0e0f84f3e1f1f08ad3dc2f487d8d12481414fef3
BizzleBo <teeninenineyt@gmail.com>
2022-01-24 20:55:32 -0800

    Script can now print a quick summary of the given article

--- scraper.py
+++ scraper.py
@@ -6,5 +6,7 @@ url="https://www.nytimes.com/live/2022/01/24/world/omicron-covid-vaccine-tests#u
 response = req.get(url, verify=False)
 soup = bs(response.content, "html.parser")
 title = soup.find("h1")
+summary = soup.find("p")
 
 print("The headline is " + title.text)
+print("A quick summary is" + summary.text)
